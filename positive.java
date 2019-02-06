eaba3760ee2cb1c966f504bfef8c66d5a23c919d
Ramyapragathi <47380576+Ramyapragathi@users.noreply.github.com>
2019-02-06 13:57:12 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class postive
+
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+	
+{
+	
+int n=3;
+	
+if(n==3)
+	
+{
+	    
+System.out.println("Postive");
+
+	}
+	
+else if(n==-3)
+	
+{
+	  
+  System.out.println("Negative");
+	
+}
+	
+else if(n==0)
+	
+{
+	   
+ System.out.println("Zero");
+	
+}
+	
+}
+
+}
+
